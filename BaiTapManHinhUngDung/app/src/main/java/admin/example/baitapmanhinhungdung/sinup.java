20dd3fe3ac90d6407cbdc97f5bb616da3dbf61ab
Ngoc Quyen <huynhngocquyen03042005@gmail.com>
2025-09-27 18:46:11 +0700

    push code logic trang đăng ký-bài tập giao diện

--- BaiTapManHinhUngDung/app/src/main/java/admin/example/baitapmanhinhungdung/sinup.java
+++ BaiTapManHinhUngDung/app/src/main/java/admin/example/baitapmanhinhungdung/sinup.java
@@ -16,11 +16,51 @@ import com.google.android.material.button.MaterialButton;
 
 public class sinup extends AppCompatActivity {
 
+    EditText editEmail, editUsername, editPassword;
+    ImageView eyeIcon;
+    MaterialButton btnSignUp;
+    boolean isPasswordVisible = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sinup);
 
+        editEmail = findViewById(R.id.editTextText);
+        editUsername = findViewById(R.id.editTextUserName1);
+        editPassword = findViewById(R.id.editTextPassword1);
+        btnSignUp = findViewById(R.id.button);
+        Toast.makeText(this, "Nút Sign Up đã được nhấn", Toast.LENGTH_SHORT).show();
+        btnSignUp.setOnClickListener(v -> {
+            Toast.makeText(this, "Nút Sign Up đã được nhấn", Toast.LENGTH_SHORT).show();
+            String email = editEmail.getText().toString().trim();
+            String username = editUsername.getText().toString().trim();
+            String password = editPassword.getText().toString().trim();
+
+            if(email.isEmpty() || username.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            SharedPreferences sharedPreferences = getSharedPreferences("MyUserPrefs", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("email", email);
+            editor.putString("username", username);
+            editor.putString("password", password);
+            editor.apply();
+
+            Toast.makeText(this, "Đăng ký thành công!", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(sinup.this, MainActivity.class); // MainActivity là màn hình Login
+            startActivity(intent);
+            finish();
+        });
+    }
+    public void goToLoginScreen(View view) {
+        Intent intent = new Intent(sinup.this, MainActivity.class); // Chắc chắn rằng MainActivity là màn hình Login
+        startActivity(intent);
+        finish();
     }
 
-    }
+
+}
