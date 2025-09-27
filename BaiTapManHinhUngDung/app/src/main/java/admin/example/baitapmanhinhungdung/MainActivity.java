0bdd4c35cc08637d01e53bbbabafe2509bf7476e
Ngoc Quyen <huynhngocquyen03042005@gmail.com>
2025-09-27 18:42:12 +0700

    push code giao diện trang đăng ký-bài tập giao diện

---
+++ BaiTapManHinhUngDung/app/src/main/java/admin/example/baitapmanhinhungdung/MainActivity.java
@@ -0,0 +1,14 @@
+package admin.example.baitapmanhinhungdung;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+}
---
+++ BaiTapManHinhUngDung/app/src/main/java/admin/example/baitapmanhinhungdung/sinup.java
@@ -0,0 +1,26 @@
+package admin.example.baitapmanhinhungdung;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class sinup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sinup);
+
+    }
+
+    }
