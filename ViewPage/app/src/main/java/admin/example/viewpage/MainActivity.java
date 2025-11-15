61acd530c319c35e9055b3a8b890e2cc80ff4d7d
Ngoc Quyen <huynhngocquyen03042005@gmail.com>
2025-11-15 08:16:34 +0700

    update bài tập ViewPage

---
+++ ViewPage/app/src/main/java/admin/example/viewpage/MainActivity.java
@@ -0,0 +1,35 @@
+package admin.example.viewpage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+    private ViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        // Gắn TabLayout + ViewPager2
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> {
+                    if (position == 0) tab.setText("Tab 1");
+                    else tab.setText("Tab 2");
+                }).attach();
+    }
+}
